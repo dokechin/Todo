34338575245e5412324406e9fa23a2b39cc47572
dokechin <user@user-PC.(none)>
2014-09-28 09:03:15 +0900

    add debug codes more

--- validators.js
+++ validators.js
@@ -2,7 +2,7 @@ var validators = (function () {
     var emailRE = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     return {
         nameValidator: function (val) {
-            this.validation.name = !!val
+            this.validation.name = !!val;
             console.log("validators" + val + !!val);
             return val
         },
